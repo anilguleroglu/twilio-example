c049df1dbefd62ab7cc4ffbf72404b0db9dccfe2
Görkem Tosun <grkkmm1@gmail.com>
2021-08-17 14:35:52 +0300

    Update index.js

--- index.js
+++ index.js
@@ -7,7 +7,19 @@ const app = express()
 app.use(express.json());
 
 const app = express();
-app.use(cors({ origin: ['http://localhost:8080', 'https://crm.dev.fowapps.com'] }));
+
+const whitelist = ['http://localhost:8080', 'https://crm.dev.fowapps.com/']
+const corsOptions = {
+  origin: (origin, callback) => {
+    if (whitelist.indexOf(origin) !== -1) {
+      callback(null, true)
+    } else {
+      callback(new Error())
+    }
+  }
+}
+
+app.use(cors(corsOptions));
 
 const port = 3000
 
