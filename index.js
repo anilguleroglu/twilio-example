5d924cc3341b3c4758a307d2cca93760bdd48927
anilguleroglu <aguleroglu@users.noreply.github.com>
2021-08-17 14:07:41 +0300

    first commit

---
+++ index.js
@@ -0,0 +1,56 @@
+var axios = require('axios').default;
+var express = require('express');
+var querystring = require('querystring');
+
+const app = express()
+app.use(express.json());
+const port = 3000
+
+app.get('/calls', async (req, res) => {
+    var response = await axios.get("https://api.twilio.com/2010-04-01/Accounts/" + req.headers.accountsid + "/Calls.json?To=" + req.query.To, {
+        auth: {
+            username: req.headers.username,
+            password: req.headers.password
+        }
+    })
+    res.json(response.data);
+})
+
+app.post('/calls', async (req, res) => {
+    const client = require('twilio')(req.headers.accountsid, req.headers.password);
+    var message = req.body.message || "Hello World";
+    var response = await client.calls.create({
+        twiml: '<Response><Say>'+message+'</Say></Response>',
+        to: req.body.to,
+        from: req.body.from
+      })
+
+    res.json(response);
+})
+
+app.post('/messages', async (req, res) => {
+    const client = require('twilio')(req.headers.accountsid, req.headers.password);
+    var message = req.body.message || "Hello World";
+    var response = await client.messages.create({
+        body:req.body.message || "Hello World",
+        to: req.body.to,
+        from: req.body.from
+      })
+
+    res.json(response);
+})
+
+app.get('/messages', async (req, res) => {
+    var response = await axios.get("https://api.twilio.com/2010-04-01/Accounts/" + req.headers.accountsid + "/Messages.json?To=" + req.query.To, {
+        auth: {
+            username: req.headers.username,
+            password: req.headers.password
+        }
+    })
+    res.json(response.data);
+})
+
+app.listen(port, () => {
+    console.log(`Example app listening at http://localhost:${port}`)
+})
+
